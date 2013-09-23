b780f544a85f7cf641c163f6292166014f04cc74
Vadim Patsalo <patsalov@gmail.com>
2013-09-23 12:16:19 -0700

    Perl script makes symbolic links in ~.

---
+++ 00_make_ln.pl
@@ -0,0 +1,16 @@
+#!/usr/bin/env perl
+
+use warnings;
+use strict;
+
+use 5.010;
+
+my @dotfiles = glob "_*";
+
+foreach my $file (@dotfiles) {
+	my $file_sub;
+	($file_sub = $file) =~ s/^_/\./;
+
+	my $ln_command = "ln -sf $file ~/$file_sub";
+	say $ln_command and system $ln_command;
+}
