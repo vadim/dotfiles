7f749541eda8117d1eee0280a8447a3ef7c81ccb
Vadim Patsalo <patsalov@gmail.com>
2018-04-20 11:13:45 -0700

    Add ignored files to symlink creator.

--- 00_make_ln.pl
+++ 00_make_ln.pl
@@ -6,7 +6,11 @@ use strict;
 my @dotfiles = glob "*";
 
 foreach my $file (@dotfiles) {
-	next if $file =~ m/.pl$/;
+	next if $file =~ /.pl$/;
+	next if $file =~ /.bash$/;
+	next if $file =~ /tmux_completion/;
+	next
+
 	my $ln_command = "ln -sf ~/dotfiles/$file ~/.$file";
 	print "$ln_command\n" and system $ln_command;
 }
